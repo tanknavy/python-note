12c2b97ce0a8f4defee1899a624739f24d459f2f
tanknavy <cheng_alex@126.com>
2020-09-26 10:04:47 -0700

    python summary init

---
+++ ETL/data_feature.py
@@ -0,0 +1,14 @@
+from sklearn.preprocessing import LabelEncoder, StandardScaler,Imputer
+from matplotlib import pyplot as plt
+from sklearn.ensemble import forest
+from sklearn.tree import export_graphviz
+
+def set_plot_sizes(sml,med,big):
+    plt.rc('font', size=sml)
+    plt.rc('axis', titlesize=sml)
+
+def parallel_trees(m, fn, n_jobs=8):
+    return list()
+
+
+def combine
